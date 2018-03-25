f683f6a38e309ba7247f69810286645c0a8ac666
Apoorv-Ag <agrawal.av27@gmail.com>
2018-03-25 23:22:08 +0530

    first Commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Form = require('./components/Form.jsx');
+ReactDOM.render(<Form />, document.getElementById('form'));
+ReactDOM.render(<Form />, document.getElementById('form1'));
+ReactDOM.render(<Form />, document.getElementById('form2'));
